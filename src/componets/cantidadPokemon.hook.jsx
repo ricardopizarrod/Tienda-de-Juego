458fd1a600e5caae2b38ad8fab1454e1ce470b9c
Ricardo Pizarro <ricardopizarro@MacBook-Pro.local>
2020-02-11 23:38:21 -0300

    EP 15 Correccion compra y venta

--- src/componets/cantidadPokemon.hook.jsx
+++ src/componets/cantidadPokemon.hook.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const CantidadPokemonHook = () => {
+
+    const game_shop = useSelector((state) => state.game_shop);
+
     return (
         <React.Fragment>
-            Unidades: 0
+            Unidades: {game_shop.pokemon}
         </React.Fragment>
     );
 }
